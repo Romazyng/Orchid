31221e88e6848860a2bd64321e3e9ced2d4f303d
Roman Chernetsky <misteryo.wins@gmail.com>
2024-12-11 11:15:56 +0300

    main page with styles and navbar

---
+++ app/ui/Navbar.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import { Marcellus_SC } from 'next/font/google';
+
+const marcellus = Marcellus_SC({
+  subsets: ['latin'], 
+  weight: '400',      
+  variable: '--font-marcellus', 
+});
+
+export default function Navbar() {
+    return (
+        <nav className="fixed top-10 left-1/2 transform -translate-x-1/2 w-[325px] h-[55px] bg-[#454040] text-white rounded-[36px]">
+  <div className="container mx-auto flex justify-between items-center">
+    <div className="bg-[#3A3737] rounded-[36px] w-[106px] h-[55px] flex items-center justify-center">
+      <Link href="/" className={`${marcellus.className} antialiased text-2xl`}>
+        Orchid
+      </Link>
+    </div>
+    <ul className="hidden md:flex items-center space-x-4"> {/* Увеличен space-x */}
+      <li className="flex items-center justify-center">
+        <Link href="/home" className="text-center">
+          Contact us
+        </Link>
+      </li>
+      <li className="bg-[#CCD0CF] rounded-[36px] w-[106px] h-[55px] flex items-center justify-center text-black">
+        <Link href="/about" className="text-center">
+          Sign in
+        </Link>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+      
+      );
+}
