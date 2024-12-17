5b4b2c30b060dd57cf98638c934970828b58ec77
Roman Chernetsky <misteryo.wins@gmail.com>
2024-12-17 12:55:40 +0300

    hot fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from 'next/font/google';
 import "./ui/globals.css";
+
 export const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -10,13 +11,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
-        className={`${inter.className} ${inter .className} antialiased`}
+        className={`${inter.className} antialiased`}
       >
         {children}
       </body>
